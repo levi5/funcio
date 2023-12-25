2716fac197a977613cf2e34b6f9dcb27730735d3
Levi Araújo <leviprudenciont@gmail.com>
2023-12-25 14:07:33 -0300

    refact: adjust pipe types

--- src/@Types/Pipe/index.ts
+++ src/@Types/Pipe/index.ts
@@ -1,26 +1,41 @@
-export interface IPipe {
-  <A>(input: A): A;
-  <A, B>(input: A, fn1: (input: A) => B): B;
-  <A, B, C>(input: A, fn1: (input: A) => B, fn2: (input: B) => C): C;
-  <A, B, C, D>(
-    input: A,
-    fn1: (input: A) => B,
-    fn2: (input: B) => C,
-    fn3: (input: C) => D
-  ): D;
-  <A, B, C, D, E>(
-    input: A,
-    fn1: (input: A) => B,
-    fn2: (input: B) => C,
-    fn3: (input: C) => D,
-    fn4: (input: D) => E
-  ): E;
-  <A, B, C, D, E, F>(
-    input: A,
-    fn1: (input: A) => B,
-    fn2: (input: B) => C,
-    fn3: (input: C) => D,
-    fn4: (input: D) => E,
-    fn5: (input: F) => F
-  ): F;
-}
+/**
+ * Type FN represents a function that accepts any number of arguments
+ * and returns any value.
+ */
+export type FN = (...args: any[]) => any;
+
+/**
+ * Type FnsMatchPipe checks if a sequence of functions can be piped together.
+ * Returns true if all functions can be piped, otherwise returns never.
+ */
+type FnsMatchPipe<FNS extends FN[]> =
+  // If there is only one function in the sequence, return true.
+  1 extends FNS['length']
+    ? boolean
+    : FNS extends [
+      infer FN1st extends FN,
+      infer FN2nd extends FN,
+      ...infer FNRest extends FN[]
+    ]
+      ? // Check if the output type of the first function matches the input type of the next.
+        Parameters<FN2nd> extends [ReturnType<FN1st>]
+          ? FnsMatchPipe<[FN2nd, ...FNRest]> // Continue checking for the remaining functions.
+          : never
+      : never;
+
+/**
+ * Type Pipeline checks if a sequence of functions can be piped together.
+ * If yes, returns a new function type representing the composition of the functions.
+ */
+export type Pipeline<FNS extends FN[]> =
+  boolean extends FnsMatchPipe<FNS>
+    ? 1 extends FNS['length']
+      ? FNS[0] // If there's only one function, return that function directly.
+      : FNS extends [
+        infer FNFIRST extends FN,
+        ...FN[],
+        infer FNLAST extends FN
+      ]
+        ? (...args: Parameters<FNFIRST>) => ReturnType<FNLAST> // Return the resulting function type from composition.
+        : never
+    : never;
