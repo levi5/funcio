74d3fdd94266f7dd233a947924c251e957b981c2
Levi Araújo <leviprudenciont@gmail.com>
2024-11-16 17:06:34 -0300

    hotfix: Add null and undefined checks in the 'map' method of the Just class

--- test/maybe.spec.ts
+++ test/maybe.spec.ts
@@ -8,6 +8,18 @@ describe("Maybe Monad", () => {
     expect(container.isJust()).toBeTruthy()
   })
 
+
+  it("Should return 'Nothing' when mapping over an undefined value", async () => {
+    const container = _Maybe.of({ status: true, error: null })
+      .map(data => data.error)
+      .map(error => error.message)
+      .map(message => message);
+
+    expect(container.isNothing()).toBeTruthy();
+  });
+
+
+
   it("Should check if the returned value is a 'Nothing' container", async () => {
     const container = _Maybe.of(null)
     expect(container.isNothing()).toBeTruthy()
@@ -20,7 +32,7 @@ describe("Maybe Monad", () => {
   })
 
 
-  it("Should add two to the value inside a Maybe object and unwrap it",()=>{
+  it("Should add two to the value inside a Maybe object and unwrap it", () => {
     const addTwo = (value: number,) => value + 2
     const value = _Maybe.of(_Maybe.of(3).map(addTwo)).unwrap()
     expect(value).toBe(5)
