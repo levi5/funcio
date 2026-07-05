bccad5a5e15a9db4542d73ae166eb56a8ea90cbc
Levi Araújo <leviprudenciont@gmail.com>
2026-07-05 12:15:43 -0300

    feat(Either): add map, mapLeft and flatMap methods to Either monad

--- test/either.spec.ts
+++ test/either.spec.ts
@@ -48,4 +48,43 @@ describe("Either monad", () => {
     const value = _unwrap<null, Object>(wrapper)
     expect(value).toStrictEqual(fakeData())
   })
+
+  it("Should map the value inside a Right", () => {
+    const value = right<string, number>(2)
+      .map(value => value * 3)
+      .getOrElse(0)
+
+    expect(value).toBe(6)
+  })
+
+  it("Should not map the value inside a Left", () => {
+    const value = left<string, number>("Invalid value")
+      .map(value => value * 3)
+      .getOrElse(0)
+
+    expect(value).toBe(0)
+  })
+
+  it("Should map the error inside a Left", () => {
+    const value = left<string, number>("Invalid value")
+      .mapLeft(error => `Error: ${error}`)
+
+    expect(value.value).toBe("Error: Invalid value")
+  })
+
+  it("Should flatMap the value inside a Right", () => {
+    const value = right<string, number>(2)
+      .flatMap(value => right<string, number>(value * 4))
+      .getOrElse(0)
+
+    expect(value).toBe(8)
+  })
+
+  it("Should not flatMap the value inside a Left", () => {
+    const value = left<string, number>("Invalid value")
+      .flatMap(value => right<string, number>(value * 4))
+      .getOrElse(0)
+
+    expect(value).toBe(0)
+  })
 })
